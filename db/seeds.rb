9278902c898ba47af11a1b922006d39de26cdd20
Yuki Kaito <ykaito21@gmail.com>
2018-09-09 22:40:17 +0900

    remove some users in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,43 +12,45 @@ Campaign.destroy_all
 Organization.destroy_all
 User.destroy_all
 
+# only need 2 users, owner and normal
+
 puts 'Creating users...'
-starbucks = User.create!(email: "starbucks@example.com",
+owner = User.create!(email: "owner@example.com",
   password: "password")
-hulu = User.create!(email: "hulu@example.com",
+normal = User.create!(email: "normal@example.com",
   password: "password")
-donqui = User.create!(email: "donqui@example.com",
-  password: "password")
-uniqlo = User.create!(email: "uniqlo@example.com",
-  password: "password")
-konika = User.create!(email: "konika@example.com",
-  password: "password")
-ichiran = User.create!(email: "ichiran@example.com",
-  password: "password")
+# donqui = User.create!(email: "donqui@example.com",
+#   password: "password")
+# uniqlo = User.create!(email: "uniqlo@example.com",
+#   password: "password")
+# konika = User.create!(email: "konika@example.com",
+#   password: "password")
+# ichiran = User.create!(email: "ichiran@example.com",
+#   password: "password")
 
 puts 'Creating organizations...'
 starbucks_o = Organization.create!(name: 'Starbucks Coffee Japan',
-                                   user: starbucks,
+                                   user: owner,
                                   description: '1971年、スターバックスはアメリカ北西部のシアトルにコーヒー豆の専門店として誕生しました。現在では世界各国の街角で一杯のコーヒーとともにくつろぎのひとときをご提供しています。',
                                   remote_org_image_url: 'http://res.cloudinary.com/dgugqcsvk/image/upload/v1535703015/243_starbucks_01.jpg')
 hulu_o = Organization.create!(name: 'Hulu',
-                              user: hulu,
+                              user: owner,
                               description: 'Huluは40,000本以上の映画・ドラマ・アニメを楽しめるオンライン動画配信サービスです。月額933円（税抜）でいつでも、どこでも、全ての作品が見放題。',
                               remote_org_image_url: 'http://res.cloudinary.com/dgugqcsvk/image/upload/v1535705968/hulu_logo.jpg')
 donqui_o = Organization.create!(name: 'ドン・キホーテグループ',
-                                user: donqui,
+                                user: owner,
                                 description: '「コンビニエンス＋ディスカウント＋アミューズメント」の三位一体を店舗コンセプトとする総合ディ スカウントストアで、約4～6万点のアイテムを取り揃えています。',
                                 remote_org_image_url: 'http://res.cloudinary.com/dgugqcsvk/image/upload/v1535987821/donqui_brand.jpg')
 uniqlo_o = Organization.create!(name: 'UNIQLO',
-                                user: uniqlo,
+                                user: owner,
                                 description: 'あらゆる人が良いカジュアルを着られるようにする株式会社ユニクロ。あなたにぴったりの1着が見つかる！',
                                 remote_org_image_url: 'http://res.cloudinary.com/dgugqcsvk/image/upload/v1535987298/B9wQNv90_400x400.jpg')
 konika_o = Organization.create!(name: 'コニカミノルタプラネタリウム',
-                                user: konika,
+                                user: owner,
                                 description: '大切な人に星空を―コニカミノルタプラネタリウムは、リラクゼーションやエンターテイメントをお届けする、大人のためのプラネタリウム。',
                                 remote_org_image_url: 'http://res.cloudinary.com/dgugqcsvk/image/upload/v1535987222/kmp_logo.jpg')
 ichiran_o = Organization.create!(name: '一蘭',
-                                user: ichiran,
+                                user: owner,
                                 description: '一蘭は、世界一とんこつラーメンを研究する会社として、これからもお客様が満足してくださる“本場本物”のとんこつラーメンをご提供できるように、しっかりと味を守りながらも、進化し続けてまいります。',
                                 remote_org_image_url:'http://res.cloudinary.com/dgugqcsvk/image/upload/v1535986883/ichiran_logo.jpg')
 
